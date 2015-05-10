1615641dadedd5b52ab59d8d9c0f43451dc0c508
Ben Balter <ben.balter@github.com>
2015-05-10 16:00:52 -0400

    update load_data call

--- app/models/content_delivery_network.rb
+++ app/models/content_delivery_network.rb
@@ -1,9 +1,9 @@
 class ContentDeliveryNetwork < ActiveRecord::Base
 
   SHORT_NAME = :cdn
-  
+
   def self.names
-    SiteInspector.load_data(:cdn).keys
+    SiteInspector::Endpoint::Dns.send(:load_data, :cdn).keys
   end
 
   validates :name, inclusion: { in: ContentDeliveryNetwork.names }, uniqueness: true, presence: true
